56dacac4770b95d438ecc871ca1427c262b43a54
Tim Griffith <grifmang@gmail.com>
2020-07-04 19:09:01 -0400

    seed edit

--- data/seeds/02 - queue.js
+++ data/seeds/02 - queue.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('prompt_queue').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('prompt_queue').insert([
         {id: 1, queue: ''}
       ]);
     });
