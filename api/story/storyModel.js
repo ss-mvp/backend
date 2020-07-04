4509607e0e7851db0cc860aa1bffe9a3e9cf6aa2
Tim Griffith <grifmang@gmail.com>
2020-07-04 16:43:51 -0400

    edit

--- api/story/storyModel.js
+++ api/story/storyModel.js
@@ -41,7 +41,7 @@ async function getPrompt() {
     return []
   } else {
     const queue_id = queue[queue.length - 1];
-    console.log(Number(queue_id.queue[0]))
+    console.log(Number(queue_id.queue))
     return getPromptById(queue_id);
   }
 }
