defe4172a75503c75884cd342b587aa236beeaa9
vitalii1120 <50341414+vitalii1120@users.noreply.github.com>
2020-08-25 08:38:43 +0300

    dev env postgresql

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgresql://postgres:1234@localhost:5432/postgres',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
