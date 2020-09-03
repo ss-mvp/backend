a688fa5e19163893502daa1740069838274d5081
Oliver17! <alexandercsierra@gmail.com>
2020-09-03 15:27:47 -0400

    cors add auth

--- api/server.js
+++ api/server.js
@@ -146,7 +146,7 @@ if(process.env.BE_ENV === 'development'){
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()
   })
 
