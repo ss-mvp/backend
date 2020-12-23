de52ff9ad6b80c2edb4a6742893f704e0e514aa5
Ben <bigriffin94@outlook.com>
2020-12-23 12:36:27 -0800

    Removed Cron job where not being used

--- api/leaderboard/leaderboard.model.js
+++ api/leaderboard/leaderboard.model.js
@@ -1,15 +1,13 @@
 const db = require("../../data/dbConfig.js");
-const CronJob = require("cron").CronJob;
 
 const getLeaderboard = () => {
-    return db("leaderboard")
-        .select("u.id", "image", "username")
-        .sum('s.score as score')
-        .from('submissions as s')
-        .join('users as u', 'u.id', 's.userId')
-        .groupBy('u.id', 's.image')
-        .orderBy('score', 'desc');
+  return db("leaderboard")
+    .select("u.id", "image", "username")
+    .sum("s.score as score")
+    .from("submissions as s")
+    .join("users as u", "u.id", "s.userId")
+    .groupBy("u.id", "s.image")
+    .orderBy("score", "desc");
 };
-
 
 module.exports = { getLeaderboard };
