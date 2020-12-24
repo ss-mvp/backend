a3538526b785b74d8c4ebcd39902cc0c3d2762a3
Ben <bigriffin94@outlook.com>
2020-12-23 22:00:06 -0800

    All formatting... and some file name changes to match the repo, made sure the SQL query was pulling from the submissions table tested and returning leaderboard users

--- api/leaderboard/leaderboardModel.js
+++ api/leaderboard/leaderboardModel.js
@@ -1,13 +1,13 @@
 const db = require("../../data/dbConfig.js");
 
-const getLeaderboard = () => {
-  return db("leaderboard")
+module.exports = { getLeaderboard };
+
+async function getLeaderboard() {
+  return await db("submissions")
     .select("u.id", "image", "username")
     .sum("s.score as score")
     .from("submissions as s")
     .join("users as u", "u.id", "s.userId")
     .groupBy("u.id", "s.image")
     .orderBy("score", "desc");
-};
-
-module.exports = { getLeaderboard };
+}
