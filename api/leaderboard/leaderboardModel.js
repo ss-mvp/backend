718cecb577eeae624a46ae1390e6dd939e86ce6e
Ben <bigriffin94@outlook.com>
2020-12-28 17:43:08 -0800

    Added a limit to the leaderboard so we are only pulling 10 at a time

--- api/leaderboard/leaderboardModel.js
+++ api/leaderboard/leaderboardModel.js
@@ -2,13 +2,15 @@ const db = require("../../data/dbConfig.js");
 
 module.exports = { getLeaderboard };
 
-async function getLeaderboard() 
-{
-    return await db("submissions")
-        .select("u.id", "image", "username")
-        .sum("s.score as score")
-        .from("submissions as s")
-        .join("users as u", "u.id", "s.userId")
-        .groupBy("u.id", "s.image")
-        .orderBy("score", "desc");
+// This function sends a list of the top ten scores to the front end to be displayed in the leaderboard. 
+
+async function getLeaderboard() {
+  return await db("submissions")
+    .select("u.id", "image", "username")
+    .sum("s.score as score")
+    .from("submissions as s")
+    .join("users as u", "u.id", "s.userId")
+    .groupBy("u.id", "s.image")
+    .orderBy("score", "desc")
+    .limit(10);
 }
