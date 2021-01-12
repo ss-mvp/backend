ca019658ce92b14140b90e69c9c794777b7a7299
Ben <bigriffin94@outlook.com>
2021-01-12 15:51:42 -0800

    Updated global var to local

--- api/leaderboard/leaderboardModel.js
+++ api/leaderboard/leaderboardModel.js
@@ -1,18 +1,14 @@
 const db = require("../../data/dbConfig.js");
 
 module.exports = { getLeaderboard };
-
-
-// Time interval to sum top 10 with thee users past 7 submissions
-const today = new Date();
-const exp = new Date(today);
-const useMe = exp.setDate(today.getDate() - 7)
-
-
 // This function sends a list of the top ten scores to the front end to be displayed in the leaderboard. 
 // We want to retrieve submissions that were submitted from today up to 7 days ago
 async function getLeaderboard() 
 {
+    // Time interval to sum top 10 with thee users past 7 submissions
+    const today = new Date();
+    const exp = new Date(today);
+    const useMe = exp.setDate(today.getDate() - 7)
     return  await db("submissions as s")
         .join("users as u", "u.id", "s.userId")
         .select("u.id", "username")
