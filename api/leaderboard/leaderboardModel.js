fca0b70a9c56db773299a54a4757f2cc41023f42
Ben <bigriffin94@outlook.com>
2021-01-12 15:00:07 -0800

    Updated leaderbaord model: pull top scored submissions from the past 7 days

--- api/leaderboard/leaderboardModel.js
+++ api/leaderboard/leaderboardModel.js
@@ -2,13 +2,20 @@ const db = require("../../data/dbConfig.js");
 
 module.exports = { getLeaderboard };
 
-// This function sends a list of the top ten scores to the front end to be displayed in the leaderboard. 
 
+// Time interval to sum top 10 with thee users past 7 submissions
+const today = new Date();
+const exp = new Date(today);
+const useMe = exp.setDate(today.getDate() - 7)
+
+
+// This function sends a list of the top ten scores to the front end to be displayed in the leaderboard. 
 async function getLeaderboard() 
 {
     return await db("submissions as s")
         .join("users as u", "u.id", "s.userId")
         .select("u.id", "username")
+        .where("s.image", ">", `${useMe}`)
         .sum("s.score as score")
         .groupBy("u.id")
         .orderBy("score", "desc")
